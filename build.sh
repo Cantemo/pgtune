099a3aa4ae075151e6a1fa996fd1b697d96f4eaa
Stefan Katzman <stefan.katzman@cantemo.com>
2020-07-28 11:30:31 +0200

    Refs #20166 - Drop support for centos6 and add support for centos8

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #! /bin/bash -xe
 
-docker build -t pgtunebuild:el6 -f Dockerfile-el6 .
 docker build -t pgtunebuild:el7 -f Dockerfile-el7 .
+docker build -t pgtunebuild:el8 -f Dockerfile-el8 .
 
 docker run --rm --user $(id -u):$(id -g) -v $PWD:$PWD -w $PWD pgtunebuild:el7 rpmbuild -D "%buildnumber $BUILD_NUMBER" -D "%_topdir $PWD/buildroot" -ba pgtune.spec
-docker run --rm --user $(id -u):$(id -g) -v $PWD:$PWD -w $PWD pgtunebuild:el6 rpmbuild -D "%buildnumber $BUILD_NUMBER" -D "%_topdir $PWD/buildroot" -ba pgtune.spec
+docker run --rm --user $(id -u):$(id -g) -v $PWD:$PWD -w $PWD pgtunebuild:el8 rpmbuild -D "%buildnumber $BUILD_NUMBER" -D "%_topdir $PWD/buildroot" -ba pgtune.spec
 
 mv buildroot/RPMS/*/*.rpm .
 
