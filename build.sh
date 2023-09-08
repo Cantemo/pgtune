6b01695a9df5c3a1209703cba889cab5f2a80c29
Johan Westerlund <johan.westerlund@codemill.se>
2023-09-08 10:40:28 +0200

    el9 build

--- build.sh
+++ build.sh
@@ -2,9 +2,11 @@
 
 docker build -t pgtunebuild:el7 -f Dockerfile-el7 .
 docker build -t pgtunebuild:el8 -f Dockerfile-el8 .
+docker build -t pgtunebuild:el9 -f Dockerfile-el9 .
 
 docker run --rm --user $(id -u):$(id -g) -v $PWD:$PWD -w $PWD pgtunebuild:el7 rpmbuild -D "%buildnumber $BUILD_NUMBER" -D "%_topdir $PWD/buildroot" -ba pgtune.spec
 docker run --rm --user $(id -u):$(id -g) -v $PWD:$PWD -w $PWD pgtunebuild:el8 rpmbuild -D "%buildnumber $BUILD_NUMBER" -D "%_topdir $PWD/buildroot" -ba pgtune.spec
+docker run --rm --user $(id -u):$(id -g) -v $PWD:$PWD -w $PWD pgtunebuild:el9 rpmbuild -D "%buildnumber $BUILD_NUMBER" -D "%_topdir $PWD/buildroot" -ba pgtune.spec
 
 mv buildroot/RPMS/*/*.rpm .
 
